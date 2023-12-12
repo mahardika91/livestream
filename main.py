919127caf907053fff64fc551de5cd3f50e08dc0
mahardika91 <itctrlr@gmail.com>
2023-12-12 00:43:08 +0000

    fixture

---
+++ main.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import json
+import datetime
+import pytz
+import os
+import subprocess
+
+def open_website_and_extract_iframe():
+    # Set up Chrome driver options for headless mode
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('start-maximized')
+    chrome_options.add_argument('disable-infobars')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+
+    # Set up Chrome driver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    # Open the URL
+    driver.get("https://scorevisit.com/")
+
+    # Initialize WebDriverWait
+    wait = WebDriverWait(driver, 10)
+
+    # Log in
+    try:
+        # Wait until the sign-in button is clickable and click it
+        sign_in_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-header.btn-signin")))
+        sign_in_button.click()
+
+        # Wait for the login form
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "signin-form")))
+
+        # Enter username and password
+        username_field = driver.find_element(By.XPATH, "//input[@type='text']")
+        password_field = driver.find_element(By.XPATH, "//input[@type='password']")
+        
+        username_field.send_keys("itctrlr@gmail.com")
+        password_field.send_keys("Ungu4150645#")
+
+        # Find and click the login button
+        login_button = driver.find_element(By.CLASS_NAME, "btn-submit")
+        login_button.click()
+
+        # Find the first "Watch Now" link
+        try:
+            watch_now_link = wait.until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Watch Now')]")))
+            link = watch_now_link.get_attribute('href')
+            driver.get(link)
+
+            # Wait for the iframe to be present
+            iframe = wait.until(EC.presence_of_element_located((By.TAG_NAME, "iframe")))
+            iframe_src = iframe.get_attribute('src')
+            
+            # Extracting the token from the URL
+            start = iframe_src.find("&token=") + len("&token=")
+            end = iframe_src.find("&is_vip=true")
+            extracted_token = iframe_src[start:end]
+
+            # Generate the current timestamp in GMT+7
+            current_time = datetime.datetime.now(pytz.timezone('Asia/Bangkok'))
+            formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+            # Filename for the JSON file
+            filename = "livestream.json"
+
+            # Check if the old token file exists and read it
+            old_token = None
+            if os.path.exists(filename):
+                with open(filename, 'r') as file:
+                    old_data = json.load(file)
+                    old_token = old_data.get("token")
+
+            # Compare and export only if tokens are different or old token is not available
+            if old_token != extracted_token:
+                output_data = {
+                    "token": extracted_token,
+                    "timestamp": formatted_time
+                }
+                with open(filename, 'w') as file:
+                    json.dump(output_data, file, indent=4)
+                print(f"New token and timestamp extracted and saved as JSON in {filename}.")
+                
+                # Git commands to add, commit, and push the changes
+                try:
+                    subprocess.run(["git", "add", "."], check=True)
+                    subprocess.run(["git", "commit", "-m", "fixture"], check=True)
+                    subprocess.run(["git", "push"], check=True)
+                    print("Changes pushed to the repository.")
+                except subprocess.CalledProcessError as e:
+                    print("An error occurred in git operations:", e)
+                    
+            else:
+                print("Token is unchanged. No new file exported.")
+
+        except Exception as e:
+            print("Error finding 'Watch Now' link or navigating to it:", e)
+
+    except Exception as e:
+        print(f"An error occurred during login: {e}")
+
+    # Close the browser
+    driver.quit()
+
+# Run the function
+open_website_and_extract_iframe()
